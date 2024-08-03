33a4d62cc3995f400d0db8521462f5cfe4767b64
oramadn <omar.ramadanpersonal@gmail.com>
2024-08-03 16:00:56 +0300

    added admin and user role and migrated

--- api/models/user.py
+++ api/models/user.py
@@ -1,7 +1,44 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        USER = 'USER', 'User'
+
     full_name = models.CharField(max_length=255)
-    role = models.CharField(max_length=50)
+    role = models.CharField(
+        max_length=5,
+        choices=Role.choices,
+        default=Role.USER,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name='api_user_set',
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='api_user_set',
+        related_query_name='user',
+    )
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_admin(self):
+        return self.role == self.Role.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == self.Role.USER
